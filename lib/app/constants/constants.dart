5e595a2716528b1e2ef366bf9912acdc318dc347
Francisco Rodriguez <franciscojrdzrdz@gmail.com>
2023-06-19 10:46:52 -0700

    New folders in project

--- lib/app/constants/constants.dart
+++ lib/app/constants/constants.dart
@@ -1,1 +1,3 @@
+import 'dart:ui';
 
+const Color accentColor = Color(0xFF0C9869);
---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkMode = ThemeData.dark();
+  static final lightMode = ThemeData.light();
+}
